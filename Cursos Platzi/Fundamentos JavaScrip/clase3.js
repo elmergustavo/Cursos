1df1595633b057aecc9f3eb157fba7f41e4d5237
Elmer Gustavo Pú <elmergustavo79@gmail.com>
2021-03-31 17:03:55 -0600

    el curso de platzi va en otro repositorio

--- Cursos Platzi/Fundamentos JavaScrip/clase3.js
+++
@@ -1,8 +0,0 @@
-var nombre = 'Gustavo', edad = 28
-
-function imprimirEdad(n,e){
-  console.log(`${n} tiene ${e} años`)
-}
-
-imprimirEdad(nombre,edad)
-imprimirEdad('Dana', 25)
--- Cursos Platzi/Fundamentos JavaScrip/clase5.js
+++
@@ -1,9 +0,0 @@
-  var nombre = 'Gustavo'
-
-  function imprimirNombreMayusculas(nombre){
-    nombre = nombre.toUpperCase()
-    console.log(nombre)
-  }
-
-  imprimirNombreMayusculas(nombre)
-  
